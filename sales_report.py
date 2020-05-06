4982e984a35ec789e5f65503d360b53c91c864c1
Kelli Budd <buddkelli1@gmail.com>
2020-05-06 16:56:16 +0000

    Initial commit

--- sales_report.py
+++ sales_report.py
@@ -4,31 +4,22 @@
 salespeople = []
 melons_sold = []
 
-# Loop over each line in file object
 f = open('sales-report.txt')
 for line in f:
-    line = line.rstrip()  # Remove trailing whitespace
-    entries = line.split('|')  # Create a list of data
+    line = line.rstrip()
+    entries = line.split('|')
 
-    # Get name of salesperson and no. of melons they've sold
     salesperson = entries[0]
     melons = int(entries[2])
 
-    # If the salesperson is already in `salespeople`, increment the no. of
-    # melons they've sold.
-    #
-    # Otherwise, add the salesperson's name to `salespeople` and
-    # `melons` to `melons_sold`
     if salesperson in salespeople:
-        # Find the position where `salesperson` is stored in `salespeople`
         position = salespeople.index(salesperson)
-        # Use that position to index into `melons_sold`
+
         melons_sold[position] += melons
     else:
         salespeople.append(salesperson)
         melons_sold.append(melons)
 
-# Loop over indices of `salespeople` and use it to index into `salespeople` and
-# `melons_sold`.
+
 for i in range(len(salespeople)):
     print(f'{salespeople[i]} sold {melons_sold[i]} melons')
